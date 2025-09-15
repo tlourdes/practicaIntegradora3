9803a4f6f80a6fbf1d767cf603eb294c88da5bef
tlourdes <tlourdes@udesa.edu.ar>
2025-09-15 10:42:27 -0300

    screen error

---
+++ proyecto/practica-react/src/screens/Error/Error.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Menu from "../../components/Navbar/Navbar";
+import Footer from "../../components/Footer/Footer";
+
+function Error() {
+   
+   return (
+      <React.Fragment>
+         <Menu />
+         <h1>Error</h1>
+         <Footer/>
+      </React.Fragment>
+   )
+    
+}
+
+export default Error;
