ceb4e64189cbd6fbd7113bd16930124bb848f09a
franciscogoni <gonif@github.com>
2025-09-16 14:06:49 -0300

    SearchResults

--- proyecto/practica-react/src/components/SearchResults/SearchResults.js
+++ proyecto/practica-react/src/components/SearchResults/SearchResults.js
@@ -1,77 +1,83 @@
 import React, { Component } from "react";
 
-class SearchResults extends Component {
+class ResultadosBusqueda extends Component {
   constructor(props) {
     super(props);
     this.state = {
       resultados: [],
-      cargando: false,
-      error: null,
     };
   }
 
-  componenteMount() {
-    this.buscarAPI();
+  componentDidMount() {
+    this.buscarResultados();
   }
 
-  componentedUpdate(prevProps) {
-    if (
-      prevProps.query !== this.props.query ||
-      prevProps.tipo !== this.props.tipo
-    ) {
-      this.buscarAPI();
+  componentDidUpdate(prevProps) {
+    if (this.props.location.search !== prevProps.location.search) {
+      this.buscarResultados();
     }
   }
 
-  buscarAPI() {
-    const { query, tipo } = this.props;
-
-    if (!query) return;
-
-    this.setState({ cargando: true, error: null });
+  buscarResultados() {
+    const parametros = new URLSearchParams(this.props.location.search);
+    const busqueda = parametros.get("query");
+    const tipo = parametros.get("type");
 
     const apiKey = "71f9dd51c9b661ac3cc8a99b148402c4"; 
-    let buscarTipo;
-    if (tipo === "pelicula") {
-    buscarTipo = "movie";
-    } else {
-    buscarTipo = "tv";
-    }
 
-    const url = `https://api.themoviedb.org/3/search/${buscarTipo}?api_key=${apiKey}&query=${encodeURIComponent(
-      query
-    )}`;
-
-    fetch(url)
+    fetch(
+      `https://api.themoviedb.org/3/search/${tipo}?api_key=${apiKey}&language=es-ES&query=${busqueda}`
+    )
       .then((res) => res.json())
-      .then((data) =>
-        this.setState({ resultados: data.results, cargando: false })
-      )
-      .catch((err) =>
-        this.setState({ error: "Error al buscar", cargando: false })
-      );
+      .then((datos) => {
+        this.setState({
+          resultados: datos.results || [],
+        });
+      })
+      .catch((err) => {
+        console.error("Error al buscar:", err);
+      });
   }
 
   render() {
-    const { resultados, cargando, error } = this.state;
+    const { resultados } = this.state;
 
-    if (cargando) return <div>Cargando...</div>;
-    if (error) return <div>{error}</div>;
-    if (!resultados || resultados.length === 0) return <div>No hay resultados</div>;
+    if (resultados.length === 0) {
+      return (
+        <div>
+          <h2>Resultados de búsqueda</h2>
+          <p>No se encontraron resultados.</p>
+        </div>
+      );
+    }
 
     return (
-      <div className="mt-4">
-        {resultados.map((item) => (
-          <div key={item.id} className="border p-2 mb-2 rounded">
-            <h3 className="font-bold">
-              {this.props.tipo === "pelicula" ? item.title : item.name}
-            </h3>
-            <p>{item.overview}</p>
-          </div>
-        ))}
+      <div>
+        <h2>Resultados de búsqueda</h2>
+        <div className="grid-resultados">
+          {resultados.map((item) => {
+            let titulo = item.title;
+            if (!titulo) {
+              titulo = item.name;
+            }
+
+            return (
+              <div key={item.id} className="card">
+                {item.poster_path && (
+                  <img
+                    src={`https://image.tmdb.org/t/p/w342${item.poster_path}`}
+                    alt={titulo}
+                  />
+                )}
+                <h3>{titulo}</h3>
+              </div>
+            );
+          })}
+        </div>
       </div>
     );
   }
 }
 
-export default SearchResults;
+export default ResultadosBusqueda;
+
