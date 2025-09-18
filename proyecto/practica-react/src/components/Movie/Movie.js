684f792f7b4331a09760144d9631c13864cc8653
tlourdes <tlourdes@udesa.edu.ar>
2025-09-18 20:31:33 -0300

    funcionalidad boton agregar mas en el detalle de series y peliculas

--- proyecto/practica-react/src/components/Movie/Movie.js
+++ proyecto/practica-react/src/components/Movie/Movie.js
@@ -1,27 +1,96 @@
-import React from 'react';
-function DetalleP({ movie }) {
-    if (!movie) return <p>Cargando...</p>
-  return (
-    <div className='sectionDetalle'>
-      <div >
-      <h2>{movie.title}</h2>
-      <img className='detalleImg'
-        src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} //Foto de la portada
-        alt={movie.title} //Titulo
-      />
-      <div className="detalleInfo">
-        <p>Rating: {movie.vote_average}</p>
-        <p>Fecha de estreno: {movie.release_date}</p>
-        <p>Duración: {movie.runtime} minutos</p>
-        <p>Sinopsis: {movie.overview}</p>
-        <p>Género: {movie.genres ? movie.genres.map(genre => genre.name).join(', ') : 'No disponible'}</p> 
-        
-        <button class='botones'>Agregar a favoritos</button>
+import React, { Component } from 'react';
+
+class DetalleP extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      favorito: false,
+    };
+  }
+
+  componentDidMount() {
+    const { movie } = this.props;
+    if (movie) {
+      let traido = localStorage.getItem("favoritosMovies");
+      if (traido !== null) {
+        let parseado = JSON.parse(traido);
+        if (parseado.includes(movie.id)) {
+          this.setState({ favorito: true });
+        }
+      }
+    }
+  }
+
+  agregarFavoritos() {
+    const { movie } = this.props;
+    let favoritos = [];
+    let traido = localStorage.getItem("favoritosMovies");
+
+    if (traido === null) {
+      favoritos.push(movie.id);
+      localStorage.setItem("favoritosMovies", JSON.stringify(favoritos));
+    } else {
+      let parseado = JSON.parse(traido);
+      if (!parseado.includes(movie.id)) {
+        parseado.push(movie.id);
+        localStorage.setItem("favoritosMovies", JSON.stringify(parseado));
+      }
+    }
+    this.setState({ favorito: true });
+  }
+
+  borrarFavoritos() {
+    const { movie } = this.props;
+    let traido = localStorage.getItem("favoritosMovies");
+
+    if (traido !== null) {
+      let parseado = JSON.parse(traido);
+      let filtrado = parseado.filter(id => id !== movie.id);
+      localStorage.setItem("favoritosMovies", JSON.stringify(filtrado));
+    }
+
+    this.setState({ favorito: false });
+  }
+
+  render() {
+    const { movie } = this.props;
+
+    if (!movie) return <p>Cargando...</p>;
+
+    return (
+      <div className='sectionDetalle'>
+        <div>
+          <h2>{movie.title}</h2>
+          <img
+            className='detalleImg'
+            src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+            alt={movie.title}
+          />
+          <div className="detalleInfo">
+            <p>Rating: {movie.vote_average}</p>
+            <p>Fecha de estreno: {movie.release_date}</p>
+            <p>Duración: {movie.runtime} minutos</p>
+            <p>Sinopsis: {movie.overview}</p>
+            <p>
+              Género: {movie.genres && movie.genres.length > 0
+                ? movie.genres.map(g => g.name).toString()
+                : "No disponible"}
+            </p>
+
+            {this.state.favorito ? (
+              <button className="botones" onClick={() => this.borrarFavoritos()}>
+                Eliminar de favoritos
+              </button>
+            ) : (
+              <button className="botones" onClick={() => this.agregarFavoritos()}>
+                Agregar a favoritos
+              </button>
+            )}
+          </div>
         </div>
-    </div>
-    </div>
-  );
+      </div>
+    );
+  }
 }
 
 export default DetalleP;
-//
--- proyecto/practica-react/src/components/Serie/Serie.js
+++ proyecto/practica-react/src/components/Serie/Serie.js
@@ -1,27 +1,96 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function DetalleS({ serie }) {
-    if (!serie) return <p>Cargando...</p>
-  return (
-    <div className='sectionDetalle'>
-      <div >
-      <h2>{serie.title}</h2>
-      <img className='detalleImg'
-        src={`https://image.tmdb.org/t/p/w500${serie.poster_path}`} //Foto de la portada
-    
-        alt={serie.name} //Titulo
-      />
-      <div className="detalleInfo">
-        <p>Rating: {serie.vote_average}</p>
-        <p>Fecha de estreno: {serie.first_air_date}</p>
-        <p>Duración: {serie.episode_run_time} minutos</p>
-        <p>Sinopsis: {serie.overview}</p>
-        <p>Género: {serie.genres ? serie.genres.map(genre => genre.name).join(', ') : 'No disponible'}</p>
-        <button class='botones'>Agregar a favoritos</button>
+class DetalleS extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      favorito: false,
+    };
+  }
+
+  componentDidMount() {
+    const { serie } = this.props;
+    if (serie) {
+      let traido = localStorage.getItem("favoritosSeries");
+      if (traido !== null) {
+        let parseado = JSON.parse(traido);
+        if (parseado.includes(serie.id)) {
+          this.setState({ favorito: true });
+        }
+      }
+    }
+  }
+
+  agregarFavoritos() {
+    const { serie } = this.props;
+    let favoritos = [];
+    let traido = localStorage.getItem("favoritosSeries");
+
+    if (traido === null) {
+      favoritos.push(serie.id);
+      localStorage.setItem("favoritosSeries", JSON.stringify(favoritos));
+    } else {
+      let parseado = JSON.parse(traido);
+      if (!parseado.includes(serie.id)) {
+        parseado.push(serie.id);
+        localStorage.setItem("favoritosSeries", JSON.stringify(parseado));
+      }
+    }
+    this.setState({ favorito: true });
+  }
+
+  borrarFavoritos() {
+    const { serie } = this.props;
+    let traido = localStorage.getItem("favoritosSeries");
+
+    if (traido !== null) {
+      let parseado = JSON.parse(traido);
+      let filtrado = parseado.filter(id => id !== serie.id);
+      localStorage.setItem("favoritosSeries", JSON.stringify(filtrado));
+    }
+
+    this.setState({ favorito: false });
+  }
+
+  render() {
+    const { serie } = this.props;
+
+    if (!serie) return <p>Cargando...</p>;
+
+    return (
+      <div className='sectionDetalle'>
+        <div>
+          <h2>{serie.title}</h2>
+          <img
+            className='detalleImg'
+            src={`https://image.tmdb.org/t/p/w500${serie.poster_path}`}
+            alt={serie.name}
+          />
+          <div className="detalleInfo">
+            <p>Rating: {serie.vote_average}</p>
+            <p>Fecha de estreno: {serie.first_air_date}</p>
+            <p>Duración: {serie.episode_run_time} minutos</p>
+            <p>Sinopsis: {serie.overview}</p>
+            <p>
+              Género: {serie.genres && serie.genres.length > 0 
+                ? serie.genres.map(g => g.name).toString() 
+                : "No disponible"}
+            </p>
+
+            {this.state.favorito ? (
+              <button className="botones" onClick={() => this.borrarFavoritos()}>
+                Eliminar de favoritos
+              </button>
+            ) : (
+              <button className="botones" onClick={() => this.agregarFavoritos()}>
+                Agregar a favoritos
+              </button>
+            )}
+          </div>
         </div>
-    </div>
-    </div>
-  );
+      </div>
+    );
+  }
 }
 
 export default DetalleS;
