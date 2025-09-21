9b44b9fc03d8e75b012574c581bd2830614ff947
franciscogoni <gonif@github.com>
2025-09-21 13:36:37 -0300

    Arregle el app.js

--- proyecto/practica-react/src/App.js
+++ proyecto/practica-react/src/App.js
@@ -1,43 +1,38 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 
+import "./css/styles.css";
 
-
-import './css/styles.css';
 // IMPORTS DE SCREENS
-import Results from "./screens/Results/Results";   
+import Home from "./screens/Home/Home";
+import Favorites from "./screens/Favorites/Favorites";
+import DetalleP from "./screens/Movie/Movie";
+import DetalleS from "./screens/Serie/Serie";
+import Results from "./screens/Results/Results";
 import Error from "./screens/Error/Error";
-import VerTodasPeliculas from "./screens/VerTodas/VerTodasPeliculas";
-import VerTodasSeries from "./screens/VerTodas/VerTodaSeries";
 import VerTodasSeriesTrending from "./screens/SeriesTrending/SeriesTrending";
 import VerTodasSeriesPopulares from "./screens/SeriesPopulares/SeriesPopulares";
 import PelisCartel from "./screens/PelisCartel/PelisCartel";
 import PelisTrending from "./screens/PelisTrending/PelisTrending";
 
-function App(){
-  return(
+function App() {
+  return (
     <>
-      
       <Switch>
         <Route path="/favoritos" exact={true} component={Favorites} />
         <Route path="/pelicula/:id" exact={true} component={DetalleP} />
         <Route path="/serie/:id" exact={true} component={DetalleS} />
         <Route path="/" exact={true} component={Home} />
+
         <Route path="/SeriesTrending" exact={true} component={VerTodasSeriesTrending} />
-        <Route path="/SeriesPopulares" exact={true} component={VerTodasSeriesPopulares} /> 
+        <Route path="/SeriesPopulares" exact={true} component={VerTodasSeriesPopulares} />
         <Route path="/PeliculasCartel" exact={true} component={PelisCartel} />
         <Route path="/PeliculasTrending" exact={true} component={PelisTrending} />
-        <Route path="" component={Error} />
-        <Route path="/VerTodasPeliculas" component={VerTodasPeliculas} />
         <Route path="/ResultadosBusqueda" component={Results} />
-        <Route path="/VerTodasSeries" component={VerTodasSeries} />
-        <Route path="/ResultadosBusqueda" component={Results} />
-        
+        <Route path="*" component={Error} />
       </Switch>
-     
-
     </>
-  )
+  );
 }
 
 export default App;
